96d82ed0c9400b97f994260400900555b74997cb
Guy Lichtman <1395797+glicht@users.noreply.github.com>
2020-11-28 17:23:19 +0200

    chore: support for setting up tags after deploy

---
+++ push-release-tag.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+#####
+# Push a tag of this release if it doesn't exist
+# CAlled after deploy. Uses a ssh deploy key: github_deploy_key
+# Key was generated: ssh-keygen -t rsa -b 4096 -C "travis-ci" -f github_deploy_key -N ''
+# and added as an encrypted file using: travis encrypt-file github_deploy_key github_deploy_key.enc -a before_deploy
+#####
+
+
+# exit on errors
+set -e
+
+PKG_VER=v$(node -p "require('./package.json').version")
+
+echo "Version from package.json: $PKG_VER"
+
+if npx git-semver-tags | grep "$PKG_VER"; then
+    echo "$PKG_VER tag already exists. Nothing to do. Skipping."
+    exit 0
+fi
+
+git config --local user.email "builds@travis-ci.com"
+git config --local user.name "Travis CI"
+git tag "$PKG_VER"
+GIT_SSH_COMMAND='ssh -i github_deploy_key -o IdentitiesOnly=yes' git push origin "$PKG_VER"
